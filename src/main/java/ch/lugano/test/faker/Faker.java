e62fc94a0e062d5e4e7fe67b0217f371b7a42420
Luca Simone <info@lucasimone.info>
2012-11-27 09:42:36 +0100

    Minor refactor

--- src/main/java/ch/lugano/test/faker/Faker.java
+++ src/main/java/ch/lugano/test/faker/Faker.java
@@ -2,6 +2,7 @@ package ch.lugano.test.faker;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 @SuppressWarnings("restriction")
 public class Faker {
@@ -9,14 +10,22 @@ public class Faker {
 	private static ScriptEngine jruby;
 	
 	static {
+		
 		jruby = new ScriptEngineManager().getEngineByName("jruby");
+
+		try {
+			jruby.eval("require 'faker'");
+		}
+		catch (ScriptException e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 		
 	public static String get(String wat) {
 		try {
-			jruby.eval("require 'faker'");
 			jruby.eval("Faker::Config.locale = 'de-ch'");
-			return (String) jruby.eval("Faker::" + wat);
+			return (String) jruby.eval(String.format("Faker::%s", wat));
 		}
 		catch (Exception ex) {
 			return "";
@@ -25,9 +34,8 @@ public class Faker {
 
 	public static String get(String wat, String locale) {
 		try {
-			jruby.eval("require 'faker'");
-			jruby.eval("Faker::Config.locale = '" + locale + "'");
-			return (String) jruby.eval("Faker::" + wat);
+			jruby.eval(String.format("Faker::Config.locale = '%s'", locale));
+			return (String) jruby.eval(String.format("Faker::%s", wat));
 		}
 		catch (Exception ex) {
 			return "";
